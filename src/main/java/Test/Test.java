8f23be20b9e6a0bd8a0bcfa43e8de0792037cc74
prajwal.khandare <prajwal.khandare@PFTBLR-LT-500.PFIN.COM>
2024-03-05 23:05:52 +0530

    1st time commit

---
+++ src/main/java/Test/Test.java
@@ -0,0 +1,18 @@
+package Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		WebDriverManager.chromedriver().driverVersion("121").setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+	}
+}
